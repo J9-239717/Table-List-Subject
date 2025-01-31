e613e3a3539e5f899e1d7fc6a3f60b3c551abc52
Jerry <teamblackextra@gmail.com>
2025-01-31 11:51:25 +0700

    Make file header store in module header

--- create chuong trinh dao tao/main.c
+++ create chuong trinh dao tao/main.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "Struct_Table.h"
-#include "Node_LOGIC.h"
-#include "LoadDT.h"
-#include "PaintTable.h"
-#include "Write_dataB.h"
-#include "functionhleperstr.h"
-#include "Score_LOGIC.h"
-#include "Read_dataB.h"
-#include "Load_input.h"
-#include "Show_Table.h"
+#include "module_table.h"
 
 // Main function
 int main() {
---
+++ create chuong trinh dao tao/module_table.h
@@ -0,0 +1,18 @@
+#ifndef _modlue_table_h_
+#define _modlue_table_h_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Struct_Table.h"
+#include "Node_LOGIC.h"
+#include "LoadDT.h"
+#include "PaintTable.h"
+#include "Write_dataB.h"
+#include "functionhleperstr.h"
+#include "Score_LOGIC.h"
+#include "Read_dataB.h"
+#include "Load_input.h"
+#include "Show_Table.h"
+
+#endif
